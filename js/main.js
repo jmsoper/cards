b26a6bdc4e3071103de9196fd04f884f3a15277a
jmsoper <jmsoper@gmail.com>
2018-04-08 23:52:21 -0400

    remove es6, continue breaking up js

--- js/main.js
+++ js/main.js
@@ -7,75 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log(game.board);
     return game;
   }
-  // generate and shuffle deck ******************************************
-  function createDeck(){
-    var deck = {};
-    var cards = getAllCards();
-    var shuffledCards = shuffleDeckInPlace(cards);
-    deck.cards = shuffledCards;
-    deck.getTopCard = function(){
-      var topCard = this.cards[0];
-      this.cards = this.cards.slice(1);
-      return topCard;
-    }
-  
-    function getAllCards(){
-      var suits = ["spades", "hearts", "clubs", "diamonds"];
-      var deck = [];
-      
-      function getCardType(cardNumber){
-        var cardFace;
-        switch (cardNumber){
-          case 1:
-            cardFace = "ace";
-            break;
-          case 11: 
-            cardFace = "jack";
-            break;
-          case 12:
-            cardFace = "queen";
-            break;
-          case 13:
-            cardFace = "king";
-            break;
-          default:
-            cardFace = cardNumber;
-        }
-        return cardFace;
-      }
-      
-      for (var i = 0; i < suits.length; i++){
-        for (var j = 1; j < 14; j++){
-          var cardFace = getCardType(j);
-          var card = { cardFace, suit: suits[i], faceUp: false };
-          deck.push(card);
-        }
-      }
-      return deck;
-    }
-    //shuffle the cards so that they're random. Use fisher-bates.
-    function shuffleDeckInPlace(cardDeck){
-      function swapCards(firstCardIdx, secondCardIdx){
-        var initialFirstCard = cardDeck[firstCardIdx];
-        cardDeck[firstCardIdx] = cardDeck[secondCardIdx];
-        cardDeck[secondCardIdx] = initialFirstCard;
-      }
-      function randomIndex(sliceStart, sliceEnd) {
-        return Math.floor(Math.random() * ( sliceEnd - sliceStart + 1)) + sliceStart;
-      }
-      
-      for (var deckIndexToBeSet = 0; deckIndexToBeSet < cardDeck.length - 1; deckIndexToBeSet++) {
-        var chosenCardIndex = randomIndex(deckIndexToBeSet, cardDeck.length - 1);
-      
-        if (chosenCardIndex !== deckIndexToBeSet){
-          swapCards(deckIndexToBeSet, chosenCardIndex);
-        }
-      }
-      
-      return cardDeck;
-    }
-    return deck;
-  }
+
   //********************************************************
   //paint the cards.
   function paintCard(givenCard){
@@ -166,6 +98,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     });
     return table;
+  }
+  
+  function render(){
+    
   }
   
   
